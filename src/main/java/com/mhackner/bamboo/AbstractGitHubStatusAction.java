9c09dc889d23b996454b1de04d0559fb587059b7
Andy Lindeman <andy@lindeman.io>
2015-11-22 16:12:44 -0500

    Runs actions, if configured, in branch plans as well as master plans

--- src/main/java/com/mhackner/bamboo/AbstractGitHubStatusAction.java
+++ src/main/java/com/mhackner/bamboo/AbstractGitHubStatusAction.java
@@ -3,19 +3,22 @@ package com.mhackner.bamboo;
 import com.atlassian.bamboo.chains.ChainExecution;
 import com.atlassian.bamboo.chains.StageExecution;
 import com.atlassian.bamboo.configuration.AdministrationConfigurationAccessor;
-import com.atlassian.bamboo.plan.Plan;
 import com.atlassian.bamboo.plan.PlanKey;
-import com.atlassian.bamboo.plan.PlanManager;
+import com.atlassian.bamboo.plan.cache.CachedPlanManager;
+import com.atlassian.bamboo.plan.cache.ImmutablePlan;
 import com.atlassian.bamboo.plugins.git.GitHubRepository;
+import com.atlassian.bamboo.plugins.git.GitRepository;
 import com.atlassian.bamboo.repository.RepositoryDefinition;
 import com.atlassian.bamboo.repository.RepositoryDefinitionManager;
 import com.atlassian.bamboo.security.EncryptionService;
 import com.atlassian.bamboo.util.Narrow;
 import com.atlassian.bamboo.utils.BambooUrl;
 import com.atlassian.bamboo.utils.SystemProperty;
-import com.google.common.base.Function;
-import com.google.common.collect.Lists;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 
+import com.google.common.collect.Lists;
+import org.apache.commons.lang.StringUtils;
 import org.kohsuke.github.GHCommitState;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
@@ -23,6 +26,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class AbstractGitHubStatusAction {
@@ -36,57 +40,63 @@ public abstract class AbstractGitHubStatusAction {
     private final EncryptionService encryptionService;
     private final BambooUrl bambooUrl;
     private final RepositoryDefinitionManager repositoryDefinitionManager;
-    private final PlanManager planManager;
+    private final CachedPlanManager cachedPlanManager;
 
     AbstractGitHubStatusAction(AdministrationConfigurationAccessor adminConfigAccessor,
                                EncryptionService encryptionService,
                                RepositoryDefinitionManager repositoryDefinitionManager,
-                               PlanManager planManager) {
+                               CachedPlanManager cachedPlanManager) {
         this.encryptionService = encryptionService;
         bambooUrl = new BambooUrl(adminConfigAccessor);
         this.repositoryDefinitionManager = repositoryDefinitionManager;
-        this.planManager = planManager;
+        this.cachedPlanManager = cachedPlanManager;
     }
 
     void updateStatus(GHCommitState status, StageExecution stageExecution) {
         ChainExecution chainExecution = stageExecution.getChainExecution();
         PlanKey planKey = chainExecution.getPlanResultKey().getPlanKey();
-        Plan plan = planManager.getPlanByKey(planKey);
+        final ImmutablePlan plan = cachedPlanManager.getPlanByKey(planKey);
         assert plan != null;
-        List<RepositoryDefinition> repos = repositoryDefinitionManager
+        final ImmutablePlan masterPlan = plan.hasMaster() ? plan.getMaster() : plan;
+
+        // Repositories are configured on the master plan. We also send status updates for branch plans if the
+        // repository URL for the branch plan matches a configured repository on the master plan.
+        List<GitHubRepository> configuredRepos = configuredGitHubReposFor(masterPlan);
+
+        List <RepositoryDefinition> planRepoDefinitions = repositoryDefinitionManager
                 .getRepositoryDefinitionsForPlan(plan);
-        String configuredRepos = plan.getBuildDefinition().getCustomConfiguration()
-                .get(Configuration.CONFIG_KEY);
-
-        if (configuredRepos == null) {
-            // TODO duplicative with Configuration
-            List<RepositoryDefinition> ghRepos = Configuration.ghReposFrom(plan);
-            configuredRepos = Lists.transform(ghRepos,
-                    new Function<RepositoryDefinition, Long>() {
-                        @Override
-                        public Long apply(RepositoryDefinition input) {
-                            return input.getId();
+        for (RepositoryDefinition planRepoDefinition : planRepoDefinitions) {
+            final GitHubRepository planRepo = Narrow.downTo(planRepoDefinition.getRepository(), GitHubRepository.class);
+            if (planRepo != null) {
+                boolean isConfigured = Iterables.any(configuredRepos, new Predicate<GitHubRepository>() {
+                    @Override
+                    public boolean apply(GitHubRepository configuredRepository) {
+                        final GitRepository configuredGitRepo = configuredRepository.getGitRepository();
+                        final GitRepository planGitRepo = planRepo.getGitRepository();
+                        if (configuredGitRepo != null && planGitRepo != null) {
+                            return StringUtils.equals(configuredGitRepo.getRepositoryUrl(),
+                                    planGitRepo.getRepositoryUrl());
+                        } else {
+                            return false;
                         }
-                    }).toString();
-        }
+                    }
+                });
 
-        for (RepositoryDefinition repo : repos) {
-            if (configuredRepos.contains(Long.toString(repo.getId()))) {
-                GitHubRepository ghRepo = Narrow.downTo(repo.getRepository(),
-                        GitHubRepository.class);
-                assert ghRepo != null; // only GitHub repos are selectable in the UI
-                String sha = chainExecution.getBuildChanges().getVcsRevisionKey(repo.getId());
-                if (sha == null) {
-                    return;
+                if (isConfigured) {
+                    String sha = chainExecution.getBuildChanges().getVcsRevisionKey(planRepoDefinition.getId());
+                    if (sha == null) {
+                        return;
+                    }
+
+                    String url = bambooUrl.withBaseUrlFromConfiguration(
+                            "/browse/" + chainExecution.getPlanResultKey());
+
+                    setStatus(status, sha, url, planRepo.getUsername(),
+                            encryptionService.decrypt(planRepo.getEncryptedPassword()),
+                            planRepo.getRepository(), stageExecution.getName());
                 }
-
-                String url = bambooUrl.withBaseUrlFromConfiguration(
-                        "/browse/" + chainExecution.getPlanResultKey());
-
-                setStatus(status, sha, url, ghRepo.getUsername(),
-                        encryptionService.decrypt(ghRepo.getEncryptedPassword()),
-                        ghRepo.getRepository(), stageExecution.getName());
             }
+
         }
     }
 
@@ -100,6 +110,32 @@ public abstract class AbstractGitHubStatusAction {
             log.info("GitHub status for commit {} ({}) set to {}.", sha, context, status);
         } catch (IOException ex) {
             log.error("Failed to update GitHub status", ex);
+        }
+    }
+
+    private List<GitHubRepository> configuredGitHubReposFor(final ImmutablePlan plan) {
+        String customConfigValue = plan.getBuildDefinition().getCustomConfiguration()
+                .get(Configuration.CONFIG_KEY);
+        if (customConfigValue != null) {
+            final List<Long> configuredRepoIds = Configuration.toList(customConfigValue);
+            final List<RepositoryDefinition> allPlanRepoDefinitions = repositoryDefinitionManager
+                    .getRepositoryDefinitionsForPlan(plan);
+
+            final ArrayList<GitHubRepository> configuredRepos = Lists.newArrayList();
+            for (RepositoryDefinition repositoryDefinition : allPlanRepoDefinitions) {
+                if (configuredRepoIds.contains(repositoryDefinition.getId())) {
+                    GitHubRepository repository = Narrow.downTo(repositoryDefinition.getRepository(),
+                            GitHubRepository.class);
+                    if (repository != null) {
+                        configuredRepos.add(repository);
+                    }
+                }
+            }
+
+            return configuredRepos;
+        } else {
+            // By default, all GitHub repos are enabled.
+            return Configuration.ghReposFor(plan);
         }
     }
 
