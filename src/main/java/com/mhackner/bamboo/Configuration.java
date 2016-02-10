b39c94f13d5d0bd323db417a226bbf78ad62fece
Michael Hackner <mhackner@gmail.com>
2016-02-10 13:28:41 -0500

    Revert "Save configuration after adding default values"
    This reverts commit 6a4825a8d35d42516dc508bb2cca127ced753b5b.

--- src/main/java/com/mhackner/bamboo/Configuration.java
+++ src/main/java/com/mhackner/bamboo/Configuration.java
@@ -1,6 +1,5 @@
 package com.mhackner.bamboo;
 
-import com.atlassian.bamboo.build.BuildDefinitionManager;
 import com.atlassian.bamboo.plan.Plan;
 import com.atlassian.bamboo.plan.TopLevelPlan;
 import com.atlassian.bamboo.plan.cache.ImmutableChain;
@@ -37,11 +36,6 @@ public class Configuration extends BaseBuildConfigurationAwarePlugin
             };
 
     private Plan plan;
-    private BuildDefinitionManager buildDefinitionManager;
-
-    public void setBuildDefinitionManager(BuildDefinitionManager buildDefinitionManager) {
-        this.buildDefinitionManager = buildDefinitionManager;
-    }
 
     @Override
     public boolean isApplicableTo(@NotNull Plan plan) {
@@ -55,7 +49,6 @@ public class Configuration extends BaseBuildConfigurationAwarePlugin
         buildConfiguration.setProperty(CONFIG_KEY, repo == null
                 ? ImmutableList.of()
                 : ImmutableList.of(repo.getId()));
-        buildDefinitionManager.savePlanAndDefinition(plan, buildConfiguration);
     }
 
     @Override
